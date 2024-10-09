c434f10303d32637d53943750d68c4423cd65229
Kalle Rantalainen <kllrantalainen366@gmail.com>
2024-10-09 15:48:40 +0300

    Added more gui elements

--- src/main/java/fi/tuni/weatheraccidentanalyzer/controllers/MainController.java
+++ src/main/java/fi/tuni/weatheraccidentanalyzer/controllers/MainController.java
@@ -5,10 +5,27 @@ import javafx.scene.control.Label;
 
 public class MainController {
     @FXML
-    private Label welcomeText;
+    private void handleDayButtonClick() {
+        System.out.println("Day button clicked!");
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void handleMonthButtonClick() {
+        System.out.println("Month button clicked!");
+    }
+
+    @FXML
+    private void handleYearButtonClick() {
+        System.out.println("Year button clicked!");
+    }
+
+    @FXML
+    private void handleDashboardButtonClick() {
+        System.out.println("Dashboard button clicked!");
+    }
+
+    @FXML
+    private void handleSettingsButtonClick() {
+        System.out.println("Settings button clicked!");
     }
 }
