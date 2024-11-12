4bafd76510bb5a3540748007b1a05eb00fc7431b
Vili Huotari <vili.huotari@gmail.com>
2024-11-12 17:36:05 +0200

    Controller and service modified to fetch yearly traffic data

--- src/main/java/fi/tuni/weatheraccidentanalyzer/controllers/TrafficAccidentsController.java
+++ src/main/java/fi/tuni/weatheraccidentanalyzer/controllers/TrafficAccidentsController.java
@@ -1,32 +1,72 @@
 package fi.tuni.weatheraccidentanalyzer.controllers;
 
-import fi.tuni.weatheraccidentanalyzer.models.TrafficAccidents;
+import fi.tuni.weatheraccidentanalyzer.models.AccidentData;
+import fi.tuni.weatheraccidentanalyzer.models.MonthlyAccidentData;
+import fi.tuni.weatheraccidentanalyzer.models.YearlyAccidentData;
 import fi.tuni.weatheraccidentanalyzer.service.TrafficService;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import java.time.YearMonth;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TrafficAccidentsController {
-  private TrafficAccidents trafficAccidents;
+  private AccidentData trafficAccidents;
   private TrafficService trafficService;
-  JSONObject obj = new JSONObject("{\"name\": \"John\"}");
-  public TrafficAccidentsController(String period) {
+
+  public TrafficAccidentsController(Integer year, String injuryType) {
     trafficService = new TrafficService();
-    
-    JSONObject jsonObject = new JSONObject(trafficService.getTrafficData(period));
+    List<AccidentData> roadUserAccidents;
+    List<MonthlyAccidentData> monthlyAccidentDataList = new ArrayList<>();
 
-    // Extract the "value" array
-    JSONArray valueArray = jsonObject.getJSONArray("value");
+    for (int month = 1; month <= 12; month++) {
+      YearMonth yearMonth = YearMonth.of(year, month);
+      String formattedMonth = yearMonth.getYear() + "M" + String.format("%02d", yearMonth.getMonthValue());
 
-    // Get the first value from the array
-    Object accidentCount = valueArray.get(0);
+      JSONObject jsonObject = new JSONObject(trafficService.getTrafficData(formattedMonth, injuryType));
 
-    trafficAccidents = new TrafficAccidents(period, (Integer) accidentCount);
-    // Print the first value
-    System.out.println("Accidents in " + trafficAccidents.getPeriod() + ":" + trafficAccidents.getAccidentCount());
+      roadUserAccidents = getRoadUserAccidents(jsonObject);
+
+      MonthlyAccidentData monthlyAccidentData = new MonthlyAccidentData(formattedMonth, roadUserAccidents);
+
+      monthlyAccidentDataList.add(monthlyAccidentData);
+
+    }
+
+    YearlyAccidentData yearlyAccidentData = new YearlyAccidentData(String.valueOf(year), monthlyAccidentDataList, "Pirkanmaa", "SSS");
+
+    for (int i = 0; i < yearlyAccidentData.getMonthlyData().size(); i++) {
+      System.out.println(yearlyAccidentData.getMonthlyData().get(i).getAccidents().get(0).getRoadUserType());
+      System.out.println(yearlyAccidentData.getMonthlyData().get(i).getAccidents().get(0).getAccidentCount());
+    }
+
   }
 
-  public TrafficAccidents getTrafficAccidents() {
+  public AccidentData getTrafficAccidents() {
     return trafficAccidents;
   }
+
+  public List<AccidentData> getRoadUserAccidents(JSONObject jsonResponse) {
+    List<AccidentData> roadUserAccidents = new ArrayList<>();
+
+    // Get the road user labels and category indices
+    JSONObject roadUserDimension = jsonResponse.getJSONObject("dimension").getJSONObject("Tienkäyttäjä");
+    JSONObject roadUserLabels = roadUserDimension.getJSONObject("category").getJSONObject("label");
+    JSONObject roadUserIndices = roadUserDimension.getJSONObject("category").getJSONObject("index");
+    
+    // Get the accident values
+    JSONArray accidentValues = jsonResponse.getJSONArray("value");
+
+    // Populate the map with road users and corresponding accident counts
+    for (String roadUserCode : roadUserLabels.keySet()) {
+        String roadUserLabel = roadUserLabels.getString(roadUserCode);
+        int index = roadUserIndices.getInt(roadUserCode); // Get the index for the current road user
+        int accidentCount = accidentValues.getInt(index); // Fetch the accident count using the index
+        AccidentData accidentData = new AccidentData(roadUserLabel, accidentCount);
+        roadUserAccidents.add(accidentData);
+    }
+
+    return roadUserAccidents;
+}
 }
--- src/main/java/fi/tuni/weatheraccidentanalyzer/service/TrafficService.java
+++ src/main/java/fi/tuni/weatheraccidentanalyzer/service/TrafficService.java
@@ -5,39 +5,95 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-
 public class TrafficService {
-
-  public String getTrafficData(String period) {
-    try {
-      HttpClient client = HttpClient.newHttpClient();
-      String requestBody = """
-        {"query": [
-            {
-                "code":"Kuukausi",
-                "selection":{
-                    "filter":"item",
-                    "values": [
+    public String getTrafficData(String yearMonth, String injuryType) {
+        try {
+            String requestBody = String.format("""
+              {
+                "query": [
+                  {
+                    "code": "Alue",
+                    "selection": {
+                      "filter": "item",
+                      "values": [
+                        "MK01"
+                      ]
+                    }
+                  },
+                  {
+                    "code": "Tienkäyttäjä",
+                    "selection": {
+                      "filter": "item",
+                      "values": [
+                        "SSS",
+                        "JK_SS",
+                        "PP_SS",
+                        "MO_SS",
+                        "MP_SS",
+                        "HA_SS",
+                        "LA_SS",
+                        "PA_SS",
+                        "KA_SS",
+                        "MU_SS",
+                      ]
+                    }
+                  },
+                  {
+                    "code": "Sukupuoli",
+                    "selection": {
+                      "filter": "item",
+                      "values": [
+                        "SSS"
+                      ]
+                    }
+                  },
+                  {
+                    "code": "Ikä",
+                    "selection": {
+                      "filter": "item",
+                      "values": [
+                        "SSS"
+                      ]
+                    }
+                  },
+                  {
+                    "code": "Kuukausi",
+                    "selection": {
+                      "filter": "item",
+                      "values": [
+                        "%s" 
+                      ]
+                    }
+                  },
+                  {
+                    "code": "Tiedot",
+                    "selection": {
+                      "filter": "item",
+                      "values": [
                         "%s"
-                    ]
+                      ]
+                    }
+                  }
+                ],
+                "response": {
+                  "format": "json-stat2"
                 }
-            }
-        ]}
-        """.formatted(period);
-        
-      HttpRequest request = HttpRequest.newBuilder()
-        .uri(URI.create("https://pxdata.stat.fi/PXWeb/api/v1/en/StatFin/ton/statfin_ton_pxt_111e.px"))
-        .header("Content-Type", "application/json")
-        .POST(HttpRequest.BodyPublishers.ofString(requestBody))
-        .build();
+              }
+            """, yearMonth, injuryType);
 
-      HttpResponse<String> response = client.send(
-        request, HttpResponse.BodyHandlers.ofString());
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://pxdata.stat.fi:443/PxWeb/api/v1/en/StatFin/ton/statfin_ton_pxt_112w.px"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
 
-      return response.body();
-    } catch (Exception e) {
-      e.printStackTrace();
-      return null;
+            HttpClient client = HttpClient.newHttpClient();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            return response.body();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
-  }
 }
