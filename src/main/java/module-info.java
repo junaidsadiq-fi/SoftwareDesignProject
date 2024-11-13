5f78516aae2d4943eb087e700003828084d086a7
Kalle Rantalainen <kllrantalainen366@gmail.com>
2024-11-13 16:12:05 +0200

    Compiling errors from the integration of APIs solved

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,9 @@ module fi.tuni.weatheraccidentanalyzer {
     requires org.kordamp.bootstrapfx.core;
     requires java.net.http;
 
+    requires java.xml;
+    requires com.google.gson;
+
     exports fi.tuni.weatheraccidentanalyzer.controllers;
     opens fi.tuni.weatheraccidentanalyzer.controllers to javafx.fxml;
     exports fi.tuni.weatheraccidentanalyzer.models;
